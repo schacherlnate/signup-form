82411dc829c64abf041241b7f0cd945af59e2bc8
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-24 17:18:07 -0600

    fix inputs starting out as invalid

--- scripts.js
+++ scripts.js
@@ -2,6 +2,13 @@ const pwd = document.querySelector("#pwd");
 const confirmPwd = document.querySelector("#cpwd");
 const confirmPwdBlock = document.querySelector("div:has(> #cpwd)");
 const errorText = document.querySelector(".error");
+const inputList = document.querySelectorAll(".data input");
+
+inputList.forEach(input => {
+    input.addEventListener("input", () => {
+        input.classList.add("invalid");
+    });
+});
 
 confirmPwdBlock.addEventListener("input", (e) => {
     if (confirmPwd.value != pwd.value) {
